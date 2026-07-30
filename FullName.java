716be4a036997c3fb37cbed15f69ec4c055e0040
laleremitcode-rgb <laleremitcode@gmail.com>
2026-07-30 09:34:40 +0530

    Create FullName.java

---
+++ FullName.java
@@ -0,0 +1,8 @@
+public class FullName {
+    public static void main(String[] args) {
+        String firstName = "Remit";
+        String lastName = "Lale";
+        String fullName = firstName + " " + lastName;
+        System.out.println("Full Name: " + fullName);
+    }
+}
